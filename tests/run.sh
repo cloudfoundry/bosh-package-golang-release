3695c4ac4243fcbff08c622f81937280250dabfd
Tanzeeb Khalili <tkhalili@pivotal.io>
2017-10-16 09:55:22 -0700

    Add option to run windows tests
    Note: must have a bosh director deployed on an IaaS that supports
    windows (ie. not bosh lite)
    
    Signed-off-by: Zachary Gershman <zgershman@pivotal.io>

--- tests/run.sh
+++ tests/run.sh
@@ -1,22 +1,39 @@
-#!/bin/bash
+#!/bin/bash -e
 
-set -e # -x
+while getopts "wl" option; do
+  case $option in
+    w)
+      export STEMCELL_NAME="bosh-google-kvm-windows2012R2-go_agent"
+      export STEMCELL_VERSION="1200.4"
+      export STEMCELL_SHA1="ef6aefa5a27fa7e378637a5875911ec3b1e44927"
+      export OS="windows2012R2"
+      export JOB_NAME="test-windows"
+      ;;
+    l)
+      export STEMCELL_NAME="bosh-warden-boshlite-ubuntu-trusty-go_agent"
+      export STEMCELL_VERSION="3445.2"
+      export STEMCELL_SHA1="7ff35e03ab697998ded7a1698fe6197c1a5b2258"
+      export OS="ubuntu-trusty"
+      export JOB_NAME="test"
+      ;;
+  esac
+done
 
 echo "-----> `date`: Upload stemcell"
-bosh -n upload-stemcell "https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent?v=3445.2" \
-  --sha1 7ff35e03ab697998ded7a1698fe6197c1a5b2258 \
-  --name bosh-warden-boshlite-ubuntu-trusty-go_agent \
-  --version 3445.2
+bosh -n upload-stemcell "https://bosh.io/d/stemcells/${STEMCELL_NAME}?v=${STEMCELL_VERSION}" \
+  --sha1 $STEMCELL_SHA1 \
+  --name $STEMCELL_NAME \
+  --version $STEMCELL_VERSION
 
 echo "-----> `date`: Delete previous deployment"
 bosh -n -d test delete-deployment --force
 
 echo "-----> `date`: Deploy"
-( set -e; cd ./..; bosh -n -d test deploy ./manifests/test.yml -v os=ubuntu-trusty -v job-name=test )
+( set -e; cd ./..; bosh -n -d test deploy ./manifests/test.yml -v os=$OS -v job-name=${JOB_NAME} )
 
 echo "-----> `date`: Run test errand"
-bosh -n -d test run-errand golang-1.8-test
-bosh -n -d test run-errand golang-1.9-test
+bosh -n -d test run-errand golang-1.8-${JOB_NAME}
+bosh -n -d test run-errand golang-1.9-${JOB_NAME}
 
 echo "-----> `date`: Delete deployments"
 bosh -n -d test delete-deployment
