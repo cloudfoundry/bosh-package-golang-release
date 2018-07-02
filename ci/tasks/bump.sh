a415788aaaa841840b43537565f9855102fc4e1b
Josh Russett <jrussett@pivotal.io>
2018-07-02 16:06:44 -0700

    Add private_yml configuration to test
    - it's required in order to create the release for testing
    
    [#158044891](https://www.pivotaltracker.com/story/show/158044891)
    
    Signed-off-by: Joshua Aresty <joshua.aresty@emc.com>

--- ci/tasks/bump.sh
+++ ci/tasks/bump.sh
@@ -8,10 +8,6 @@ source ${script_dir}/bump-helpers.sh
 cd bumped-golang-release
 git clone ../golang-release .
 
-set +x
-echo "${PRIVATE_YML}" > config/private.yml
-set -x
-
 replace_if_necessary 1.10 linux
 replace_if_necessary 1.10 darwin
 replace_if_necessary 1.10 windows
@@ -19,5 +15,3 @@ replace_if_necessary 1.10 windows
 replace_if_necessary 1.9 linux
 replace_if_necessary 1.9 darwin
 replace_if_necessary 1.9 windows
-
-bosh upload-blobs
