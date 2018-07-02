db53a563792d67660f08a580b5f294e469b4e230
Josh Russett <jrussett@pivotal.io>
2018-07-02 11:33:07 -0700

    Task changes for bumping
    [#158044891](https://www.pivotaltracker.com/story/show/158044891)
    
    Signed-off-by: Joshua Aresty <joshua.aresty@emc.com>

--- ci/tasks/bump-helpers.sh
+++ ci/tasks/bump-helpers.sh
@@ -4,7 +4,9 @@ function replace_if_necessary() {
   blobname=$(basename $(ls ../golang-${version}/*${platform}*))
   if ! bash -c "bosh blobs | grep -q ${blobname}"; then
     existing_blob=$(bosh blobs | awk '{print $1}' | grep "go${version}.*${platform}")
-    bosh remove-blob ${existing_blob}
+    if [ -n "${existing_blob}" ]; then
+      bosh remove-blob ${existing_blob}
+    fi
     bosh add-blob --sha2 ../golang-${version}/${blobname} ${blobname}
   fi
 }
--- ci/tasks/bump.sh
+++ ci/tasks/bump.sh
@@ -5,7 +5,17 @@ set -eux
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${script_dir}/bump-helpers.sh
 
+echo "${PRIVATE_YML}" > config/private.yml
+
 cd bumped-golang-release
 git clone ../golang-release .
 
-sleep 100
+replace_if_necessary 1.10 linux
+replace_if_necessary 1.10 darwin
+replace_if_necessary 1.10 windows
+
+replace_if_necessary 1.9 linux
+replace_if_necessary 1.9 darwin
+replace_if_necessary 1.9 windows
+
+bosh upload-blobs
