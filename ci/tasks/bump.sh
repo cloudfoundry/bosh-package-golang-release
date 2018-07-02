f593cee939c18d10d5d952d410ee2466524f1de3
Joshua Aresty <joshua.aresty@emc.com>
2018-07-02 11:46:50 -0700

    Write out private.yml in the right place
    Signed-off-by: Josh Russett <jrussett@pivotal.io>

--- ci/tasks/bump.sh
+++ ci/tasks/bump.sh
@@ -5,10 +5,12 @@ set -eux
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${script_dir}/bump-helpers.sh
 
-echo "${PRIVATE_YML}" > config/private.yml
-
 cd bumped-golang-release
 git clone ../golang-release .
+
+set +x
+echo "${PRIVATE_YML}" > config/private.yml
+set -x
 
 replace_if_necessary 1.10 linux
 replace_if_necessary 1.10 darwin
