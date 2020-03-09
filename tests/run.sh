28fae6140732a2d1cbc3c39c0790e1ac7178849b
Rebecca Putinski <rputinski@pivotal.io>
2020-03-09 17:22:37 -0400

    Update golang release test for +1.14 -1.12

--- tests/run.sh
+++ tests/run.sh
@@ -19,8 +19,8 @@ pushd ${script_dir}/..
 
   echo "-----> `date`: Run test errand"
   bosh -n -d test run-errand golang-1-${JOB_NAME}
-  bosh -n -d test run-errand golang-1.12-${JOB_NAME}
   bosh -n -d test run-errand golang-1.13-${JOB_NAME}
+  bosh -n -d test run-errand golang-1.14-${JOB_NAME}
 
   echo "-----> `date`: Delete deployments"
   bosh -n -d test delete-deployment
