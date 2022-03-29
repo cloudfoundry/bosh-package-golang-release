fbabc471fd94e4080e9fdf0618b4b2b92f84542e
Brian Upton <bupton@vmware.com>
2022-03-29 14:46:31 -0700

    Fix language bump script to handle order swapping
    The dev/add-line script that adds a new minor version and removes the
    deprecated older version does so by replacing the old version (1.16)
    with the new version (1.17). However, this left 1.17 as the "latest"
    version, which was not correct.

--- ci/tasks/bump.sh
+++ ci/tasks/bump.sh
@@ -16,18 +16,35 @@ set -x
 git config user.name "CI Bot"
 git config user.email "cf-bosh-eng@pivotal.io"
 
-replace_if_necessary 1.18 linux
-replace_if_necessary 1.18 darwin
-replace_if_necessary 1.18 windows
-
-if [[ "$( git status --porcelain )" != "" ]]; then
-  git commit -am "Bump to golang $(cat ../golang-1.18/.resource/version)" -m "$(cd ../golang-1.18 && ls)"
+# The dev/add-line script blindly replaces the old version being removed with the
+# new version being added. This means that the two versions may swap orders within
+# this script.
+#
+# Thus we need to determine whether the "first" or "second" version is the latest
+# version, which we need to know in order to properly set the version for the
+# golang-1-{PLATFORM} package.
+first_version="1.18"
+second_version="1.17"
+if [[ "$first_version" < "$second_version" ]]; then
+  first_version_is_latest=false
+  second_version_is_latest=true
+else
+  first_version_is_latest=true
+  second_version_is_latest=false
 fi
 
-replace_if_necessary 1.17 linux true
-replace_if_necessary 1.17 darwin true
-replace_if_necessary 1.17 windows true
+replace_if_necessary $first_version linux $first_version_is_latest
+replace_if_necessary $first_version darwin $first_version_is_latest
+replace_if_necessary $first_version windows $first_version_is_latest
 
 if [[ "$( git status --porcelain )" != "" ]]; then
-  git commit -am "Bump to golang $(cat ../golang-1.17/.resource/version)" -m "$(cd ../golang-1.17 && ls)"
+  git commit -am "Bump to golang $(cat ../golang-$first_version/.resource/version)" -m "$(cd ../golang-$first_version && ls)"
+fi
+
+replace_if_necessary $second_version linux $second_version_is_latest
+replace_if_necessary $second_version darwin $second_version_is_latest
+replace_if_necessary $second_version windows $second_version_is_latest
+
+if [[ "$( git status --porcelain )" != "" ]]; then
+  git commit -am "Bump to golang $(cat ../golang-$second_version/.resource/version)" -m "$(cd ../golang-$second_version && ls)"
 fi
