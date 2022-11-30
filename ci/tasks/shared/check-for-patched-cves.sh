ba26376105ee9f2983b90e975594896444e27283
Joseph Palermo <jpalermo@vmware.com>
2022-11-29 16:55:05 -0800

    Add task to check for fixed CVEs between versions
    This script scans for vulnerabilities on the current version of
    input_repo, then checks out the previous released version and checks
    again. It will exit non-zero if there are no fixed vulnerabilities
    between the two versions.
    
    It produces a list of the fixed CVEs, raw JSON output from the scanning
    tool, and a pre-built release notes with CVE IDs + title / description.
    
    [#183168350] Periodically create releases of "bosh releases" so that auto-bumped dependencies are available for consumption
    
    Signed-off-by: Brian Upton <bupton@vmware.com>

---
+++ ci/tasks/shared/check-for-patched-cves.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -eu
+
+version_number="$(cat version/version)"
+
+pushd "input_repo/$SOURCE_PATH"
+
+current_json="$(trivy filesystem . --severity "$SEVERITY" --security-checks vuln --format json | jq "(if .Results then .Results else [] end) | map(.Vulnerabilities) | flatten | map(select(. != null)) | unique_by(.VulnerabilityID)")"
+current_list=$(echo "$current_json" | jq -r "map(.VulnerabilityID) | sort | join(\"\\n\")")
+
+
+git checkout "v${version_number}"
+previous_json="$(trivy filesystem . --severity "$SEVERITY" --security-checks vuln --format json | jq "(if .Results then .Results else [] end) | map(.Vulnerabilities) | flatten | map(select(. != null)) | unique_by(.VulnerabilityID)")"
+previous_list=$(echo "$previous_json" | jq -r "map(.VulnerabilityID) | sort | join(\"\\n\")")
+
+popd
+
+patched_cves="$(comm -13 <(echo "$current_list") <(echo "$previous_list"))"
+
+if [ -z "$patched_cves" ]; then
+  echo "No fixed CVEs detected"
+  exit 1
+fi
+
+cve_ids=""
+for cve_id in $patched_cves; do
+  cve_ids="$cve_ids,\"$cve_id\""
+done
+cve_ids="${cve_ids:1}"
+
+patched_json="$(echo "$previous_json" | jq "map(select(.VulnerabilityID == ($cve_ids)))")"
+release_notes="$(echo "$patched_json" | jq -r "map(\"* \" + .VulnerabilityID + \": \" + if .Title then .Title else .Description end) | sort | join(\"\n\")")"
+
+echo "Fixed CVEs:"
+echo "$release_notes"
+
+echo "$patched_cves" > patched_cves/patched-cves.txt
+echo "$patched_json" > patched_cves/patched-cves.json
+echo "$release_notes" > patched_cves/release-notes.md
