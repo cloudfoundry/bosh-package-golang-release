cc30ed46170cdd459d8ec566f7cd72b1352581c0
Joseph Palermo <jpalermo@pivotal.io>
2023-01-12 15:14:10 -0800

    Update ci pipeline with new credhub entries

--- ci/configure.sh
+++ ci/configure.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-# set -ex
-#   lpass status
-# set +ex
-
 dir=$(dirname $0)
 
 fly -t ${CONCOURSE_TARGET:-bosh-ecosystem} \
   sp -p golang-release \
-  -c $dir/pipeline.yml \
-  -l <(lpass show --notes 'golang-release pipeline vars')
+  -c $dir/pipeline.yml
