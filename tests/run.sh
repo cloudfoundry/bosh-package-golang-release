5556911067b3f129201dec7dbcc8d2c2f536a689
Joseph Palermo <jpalermo@vmware.com>
2023-02-08 14:58:38 -0800

    Add Golang 1.20, remove 1.18
    [#184384439] Add golang 1.20 to bosh-packages
    
    Signed-off-by: Brian Upton <bupton@vmware.com>

--- tests/run.sh
+++ tests/run.sh
@@ -20,7 +20,7 @@ pushd ${script_dir}/..
   echo "-----> `date`: Run test errand"
   bosh -n -d test run-errand golang-1-${JOB_NAME}
   bosh -n -d test run-errand golang-1.19-${JOB_NAME}
-  bosh -n -d test run-errand golang-1.18-${JOB_NAME}
+  bosh -n -d test run-errand golang-1.20-${JOB_NAME}
 
   echo "-----> `date`: Delete deployments"
   bosh -n -d test delete-deployment
