9c5df920e6a8ce6c3ef336dbfd4b165a9df80d11
Brian Upton <bupton@vmware.com>
2023-06-22 16:53:20 -0700

    Add basic usage to get-package-version helper
    Signed-off-by: Joseph Palermo <jpalermo@vmware.com>

--- scripts/get-package-version.sh
+++ scripts/get-package-version.sh
@@ -2,5 +2,13 @@
 
 dir="$(dirname $0)"
 
+fingerprint="${1}"
+package="${2}"
+
+if [ -z "${fingerprint}" ] || [ -z "${package}" ]; then
+    echo "Usage: ${0} <fingerprint> <package-name>"
+    exit 1;
+fi
+
 cd "${dir}"
-git log -S  "${1}" --format=format:%H | head -1 | xargs -I {} git show {}:"packages/${2}/version"
+git log -S  "${fingerprint}" --format=format:%H | head -1 | xargs -I {} git show {}:"packages/${package}/version"
