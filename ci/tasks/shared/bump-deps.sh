916511bfa2350b9b6e2cb7d0d5ecc474b271c035
Daniel Felipe Ochoa <daniel-felipe.ochoa@broadcom.com>
2024-02-12 18:11:24 -0500

    Execute go mod vendor after tidying.
    When downgrading there are many paths that lead to inconsistent
    vendoring. Because of that it's better to always vendor after using go
    mod tidy.
    
    Also improves variable naming and implements some shellcheck suggestions
    
    [#187012474] Update golang bump deps script to lock toolchain to earliest supported golang toolchain rather than latest

--- ci/tasks/shared/bump-deps.sh
+++ ci/tasks/shared/bump-deps.sh
@@ -14,15 +14,15 @@ fi
 
 #intentionally cause an explicit commit if the underlying go version in our compiled Dockerfile changes.
 #assume the go-dep-bumper and bosh-utils are bumping at the same cadence.
-NEW_GO_MINOR=$(go version | sed 's/go version go1\.\([0-9]\+\)[. ].*$/\1/g')
-CURRENT_GO_MINOR=$(cat go.mod | grep -E "^go 1.*" | sed "s/^\(go 1.\)\([0-9]\+\)/\2/")
+GO_MINOR=$(go version | gsed 's/go version go1\.\([0-9]\+\)[. ].*$/\1/g')
+CURRENT_GO_MINOR=$(cat go.mod | grep -E "^go 1.*" | gsed "s/^\(go 1.\)\([0-9]\+\)/\2/")
 
-if [ -z "$GO_MINOR" ]; then
-  GO_MINOR=$((NEW_GO_MINOR-1))
+if [ -z "$NEW_GO_MINOR" ]; then
+  NEW_GO_MINOR=$((GO_MINOR-1))
 fi
 
-go mod edit -go=1.$GO_MINOR
-go mod edit -toolchain=go1.$GO_MINOR.0
+go mod edit -go=1.$NEW_GO_MINOR
+go mod edit -toolchain=go1.$NEW_GO_MINOR.0
 
 go get -u ./...
 
@@ -30,15 +30,16 @@ if [ -d ./tools ]; then
   go get -u ./tools
 fi
 
-if [ $CURRENT_GO_MINOR == $GO_MINOR ]; then
+if [ "$CURRENT_GO_MINOR" -eq "$NEW_GO_MINOR" ]; then
   go mod tidy
-elif [ "$CURRENT_GO_MINOR" -gt "$GO_MINOR" ] ; then
-  go mod tidy -go=1.$GO_MINOR
-  go mod vendor
+elif [ "$CURRENT_GO_MINOR" -gt "$NEW_GO_MINOR" ] ; then
+  go mod tidy -go=1.$NEW_GO_MINOR
 else
-  go mod tidy -go=1.$CURRENT_GO_MINOR
-  go mod tidy -go=1.$GO_MINOR
+  go mod tidy -go=1."$CURRENT_GO_MINOR"
+  go mod tidy -go=1.$NEW_GO_MINOR
 fi
+
+go mod vendor
 
 if [ "$(git status --porcelain)" != "" ]; then
   git status
@@ -46,11 +47,11 @@ if [ "$(git status --porcelain)" != "" ]; then
   [ -f go.sum ] && git add vendor go.sum
   git config user.name $GIT_USER_NAME
   git config user.email $GIT_USER_EMAIL
-  if [ $CURRENT_GO_MINOR == $GO_MINOR ]; then
+  if [ "$CURRENT_GO_MINOR" -eq "$NEW_GO_MINOR" ]; then
     git commit -m "Update vendored dependencies"
-  elif [ "$CURRENT_GO_MINOR" -gt "$GO_MINOR" ] ; then
-    git commit -m "Downgrading to go version 1.$GO_MINOR" -m "- (and update vendored dependencies)"
+  elif [ "$CURRENT_GO_MINOR" -gt "$NEW_GO_MINOR" ] ; then
+    git commit -m "Downgrading to go version 1.$NEW_GO_MINOR" -m "- (and update vendored dependencies)"
   else
-    git commit -m "Bump to go version 1.$GO_MINOR" -m "- (and update vendored dependencies)"
+    git commit -m "Bump to go version 1.$NEW_GO_MINOR" -m "- (and update vendored dependencies)"
   fi
 fi
