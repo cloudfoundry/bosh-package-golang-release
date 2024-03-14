7360ed824cb8fadaec2e1c49591af07d7340c2db
Brian Upton <bupton@vmware.com>
2024-03-14 15:58:02 -0500

    Add task to compare golang versions of binaries
    This new shared task allows you to compare the go version of a
    previously released binary against an upcoming binary. If the versions
    are different, then the script returns successful and you can trigger an
    automatic release with appropriate release notes (similar to the other
    shared `check-for` tasks)

---
+++ ci/tasks/shared/check-for-updated-binary-version.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+updated_package=0
+previous_binary="$(ls previous_binary/${PREVIOUS_BINARY_PATTERN})"
+current_binary="$(ls current_binary/${CURRENT_BINARY_PATTERN})"
+
+previous_version="$(go version "${previous_binary}" | egrep -o '[0-9]+\.[0-9]+\.[0-9]+' | tail -n 1)"
+current_version="$(go version "${current_binary}" | egrep -o '[0-9]+\.[0-9]+\.[0-9]+' | tail -n 1)"
+
+echo "Previous go version: ${previous_version}"
+echo "Current go version: ${current_version}"
+
+if [ "${current_version}" != "${previous_version}" ]; then
+  updated_package=1
+  release_notes="### Updates:"
+  release_notes="${release_notes}
+* Update to golang ${current_version}"
+fi
+
+if [ "${updated_package}" == "0" ]; then
+  echo "Golang Version Not Updated"
+  exit 1
+fi
+
+echo "$release_notes"
+
+echo "$release_notes" >> release-notes/release-notes.md
